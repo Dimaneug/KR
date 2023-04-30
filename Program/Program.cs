b928d027a7481e21ef3b2c3c0ba3f2660c221707
Dimaneug <dimaneug@gmail.com>
2023-04-30 07:32:04 +0300

    Divided function into two and added comments

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,5 @@
-﻿int ReadData(string msg) 
+﻿// Returns entered int
+int ReadData(string msg)
 {
     int res;
     while (true)
@@ -6,33 +7,32 @@
         try
         {
             Console.Write(msg);
-            res = int.Parse(Console.ReadLine()??"0");
-            break; 
+            res = int.Parse(Console.ReadLine() ?? "0");
+            break;
         }
         catch
         {
             Console.WriteLine("Нужно ввести число!");
         }
-        
+
     }
-    
+
     return res;
 }
 
-(string[], int) GenArrAndCountElemsLess4(int arrLength)
+// Method to generate array with keyboard
+string[] GenArrByKeyboard(int arrLength)
 {
     string[] arr = new string[arrLength];
-    int count = 0;
     for (int i = 0; i < arrLength; i++)
     {
         Console.Write("Введите arr[{0}]: ", i);
-        arr[i] = Console.ReadLine()??"0";
-        if (arr[i].Length < 4)
-            count++;
+        arr[i] = Console.ReadLine() ?? "0";
     }
-    return (arr, count);
+    return arr;
 }
 
+// Prints array
 void PrintArr(string[] arr)
 {
     if (arr.Length == 0)
@@ -40,21 +40,35 @@ void PrintArr(string[] arr)
         Console.WriteLine("[]");
         return;
     }
-    Console.Write("[\""+arr[0]+"\"");
+    Console.Write("[\"" + arr[0] + "\"");
     for (int i = 1; i < arr.Length; i++)
     {
-        Console.Write(", \""+arr[i]+"\"");
+        Console.Write(", \"" + arr[i] + "\"");
     }
     Console.WriteLine("]");
 }
 
-string[] ReturnArrWithElemsLessLength(int count, string[] arr)
+// Returns number of elements with length less than elemLen in arr
+int CountElemsWithLenLess(int elemLen, string[] arr)
 {
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < elemLen)
+            count++;
+    }
+    return count;
+}
+
+// Returns array with elements less than elemLen characters long in given arr
+string[] ReturnArrWithElemsLenLess(int elemLen, string[] arr)
+{
+    int count = CountElemsWithLenLess(elemLen, arr);
     string[] newArr = new string[count];
     count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length < 4)
+        if (arr[i].Length < elemLen)
         {
             newArr[count] = arr[i];
             count++;
@@ -64,7 +78,6 @@ string[] ReturnArrWithElemsLessLength(int count, string[] arr)
 }
 
 int n = ReadData("Введите размер массива: ");
-(string[] arr, int count) = GenArrAndCountElemsLess4(n);
-PrintArr(arr);
-string[] newArr = ReturnArrWithElemsLessLength(count, arr);
+string[] arr = GenArrByKeyboard(n);
+string[] newArr = ReturnArrWithElemsLenLess(4, arr);
 PrintArr(newArr);
